8495bbfb4bb550aa5926ada0c86fb79c1aa36a09
AncientMariner <xander.lysak@gmail.com>
2013-07-11 21:57:05 +0300

    facade

---
+++ Patterns/src/main/java/org/xander/Facade/HomeTheaterFacade.java
@@ -0,0 +1,49 @@
+package org.xander.Facade;
+
+public class HomeTheaterFacade {
+    Tuner tuner;
+    Player player;
+    Projector projector;
+    TheaterLights theaterLights;
+    Screen screen;
+    PopcornPopper popcornPopper;
+
+
+    public HomeTheaterFacade(Tuner tuner,
+                             Player player,
+                             Projector projector,
+                             TheaterLights theaterLights,
+                             Screen screen,
+                             PopcornPopper popcornPopper) {
+        this.tuner = tuner;
+        this.player = player;
+        this.projector = projector;
+        this.theaterLights = theaterLights;
+        this.screen = screen;
+        this.popcornPopper = popcornPopper;
+    }
+
+    public void watchMovie(String movie) {
+        System.out.println("Buckle up and get ready to watch a movie...");
+        popcornPopper.on();
+        popcornPopper.pop();
+        theaterLights.dim(15);
+        screen.down();
+        projector.on();
+        projector.wideScreenMode();
+        tuner.on();
+        player.on();
+        player.play(movie);
+    }
+
+    public void endMovie() {
+        System.out.println("\nShutting the theater down...");
+        popcornPopper.off();
+        theaterLights.on();
+        screen.up();
+        projector.off();
+        player.stop();
+        player.off();
+        tuner.off();
+    }
+}
---
+++ Patterns/src/main/java/org/xander/Facade/TheaterLights.java
@@ -0,0 +1,11 @@
+package org.xander.Facade;
+
+public class TheaterLights {
+    public void on() {
+        System.out.println("Turning on the theater lights");
+    }
+
+    public void dim(int intensity) {
+        System.out.println("Dimming theater lights to " + intensity + "% intensity");
+    }
+}
