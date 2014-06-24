8d0694c5d6dfa469d3a1017c35fb566f8e6777a6
AncientMariner <xander.lysak@gmail.com>
2014-06-24 19:09:23 +0300

    extensible enum type emulation

---
+++ Patterns/src/main/java/org/xander/Factory/factory/OperationBase.java
@@ -0,0 +1,5 @@
+package org.xander.Factory.factory;
+
+public interface OperationBase {
+    double apply(double x, double y);
+}
--- Patterns/src/main/java/org/xander/Factory/factory/OperationTest.java
+++ Patterns/src/main/java/org/xander/Factory/factory/OperationTest.java
@@ -1,5 +1,8 @@
 package org.xander.Factory.factory;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 public class OperationTest {
     public static void main(String[] args) {
         double x = Double.parseDouble(args[0]);
@@ -10,5 +13,20 @@ public class OperationTest {
         System.out.println();
         for (Operation op : Operation.values())
             System.out.printf("%f %s %f = %f%n", x, op, y, op.apply(x, y));
+
+        testBoundedTypeToken(ExtendedOperation.class, x, y);
+        testBoundedWildCardType(Arrays.asList(ExtendedOperation.values()), x, y);
+    }
+
+    private static void testBoundedWildCardType(Collection<? extends OperationBase> opSet, double x, double y) {
+        for (OperationBase op : opSet)
+            System.out.printf("%f %s %f = %f%n", x, op, y, op.apply(x, y));
+    }
+
+    private static <T extends Enum<T> & OperationBase> void testBoundedTypeToken(
+            Class<T> opSet, double x, double y) {
+        for (OperationBase op : opSet.getEnumConstants())
+            System.out.printf("%f %s %f = %f%n",
+                    x, op, y, op.apply(x, y));
     }
 }
