d291ccc88955aa139e30d480f6d4a1dae1a3b0b4
AncientMariner <xander.lysak@gmail.com>
2017-10-10 17:31:50 +0200

    Create TailCall.java

---
+++ src/main/java/org/xander/behavioral/lazyevaluation/TailCall.java
@@ -0,0 +1,15 @@
+package org.xander.behavioral.lazyevaluation;
+
+@FunctionalInterface
+public interface TailCall<T> {
+    TailCall<T> apply();
+    default boolean isComplete() { return false; }
+    default T result() { throw new Error("not implemented"); }
+    default T invoke() {
+        return Stream.iterate(this, TailCall::apply)
+                     .filter(TailCall::isComplete)
+                     .findFirst()
+                     .get()
+                     .result();
+    } 
+}
