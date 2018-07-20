ae0256987645273614ed9079d1415968909d1558
AncientMariner <xander.lysak@gmail.com>
2018-07-20 10:05:18 +0200

    Update Menu.java

--- src/main/java/org/xander/behavioral/iterator/Menu.java
+++ src/main/java/org/xander/behavioral/iterator/Menu.java
@@ -5,5 +5,5 @@ import java.util.Iterator;
 public interface Menu {
     void addItem(String name, String description, boolean vegetarian, double price);
 
-    Iterator createIterator();
+    Iterator<MenuItem> createIterator();
 }
