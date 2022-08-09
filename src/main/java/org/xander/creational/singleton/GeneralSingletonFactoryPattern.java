010fee2e18192954aa85f0ca4c180fe0ee610f53
AncientMariner <xander.lysak@gmail.com>
2022-08-09 09:02:18 +0200

    add general singleton factory pattern

---
+++ src/main/java/org/xander/creational/singleton/GeneralSingletonFactoryPattern.java
@@ -0,0 +1,27 @@
+package org.xander.creational.singleton;
+
+import java.util.function.UnaryOperator;
+
+public class GeneralSingletonFactoryPattern {
+    private static final UnaryOperator<Object> IDENTITY_FN = (t) -> t;
+    @SuppressWarnings("unchecked")
+    public static <T> UnaryOperator<T> identityFunction() { return (UnaryOperator<T>) IDENTITY_FN;
+    }
+
+    // Sample program to exercise generic singleton
+    public static void main(String[] args) {
+        String[] strings = { "jute", "hemp", "nylon" };
+        UnaryOperator<String> sameString = identityFunction();
+
+        for (String s : strings) {
+            System.out.println(sameString.apply(s));
+        }
+        
+        Number[] numbers = { 1, 2.0, 3L };
+        UnaryOperator<Number> sameNumber = identityFunction();
+
+        for (Number n : numbers) {
+            System.out.println(sameNumber.apply(n));
+        }
+    }
+}
